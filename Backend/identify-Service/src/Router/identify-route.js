ae7f7bf47eace101d211f9e30a762a5d4caaa364
Devbharath2k <devbharath21k@gmail.com>
2025-04-15 16:41:59 +0530

    identify-route

--- Backend/identify-Service/src/Router/identify-route.js
+++ Backend/identify-Service/src/Router/identify-route.js
@@ -8,7 +8,7 @@ router.post('/register', profilephotoUpload, UserController.register);
 router.post('/login', UserController.login);
 router.post('/update', VerifyToken, ResumeUpload, UserController.updateprofile);
 router.post('/forgot-password', UserController.forgotpassword);
-router.post('/verfy-password', UserController.verfiypassword);
+router.post('/verfiy-password', UserController.verfiypassword);
 router.post('/reset-password', UserController.resetpassword);
 router.post('/logout', VerifyToken, UserController.logout);
 
